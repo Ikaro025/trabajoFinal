cd4b21a739c80beee761d2b43dd502b2f3dd0c18
Luis A Z <siul.pretorius@gmail.com>
2018-08-22 18:22:47 -0400

    agregando css, ima, js al proyecto

--- index.php
+++ index.php
@@ -5,3 +5,14 @@
  * Date: 22-08-18
  * Time: 06:04 PM
  */
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+
+</body>
+</html>
